ef3de96db8103495bfd8ed6e1cd273db400facfa
Prajjwalgurung404 <aryangrg020@gmail.com>
2025-01-30 23:27:18 +0530

    First Commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      {/* home section */}
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+        </Link>
+
+        {/* our courses section on nav */}
+
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4">
+            <HoveredLink href="/web-dev">All courses</HoveredLink>
+            <HoveredLink href="/web-dev">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/web-dev">Advance Composition</HoveredLink>
+            <HoveredLink href="/web-dev">Song writing</HoveredLink>
+            <HoveredLink href="/web-dev">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+
+        {/* contact section on nav */}
+
+        <Link href={"/contactus"}>
+          <MenuItem setActive={setActive} active={active} item="Contact us">
+            Awhhh Baby
+          </MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 export default {
   content: [
@@ -6,6 +11,8 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
+
   theme: {
     extend: {
       colors: {
@@ -14,5 +21,20 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // rest of the code
+    addVariablesForColors,
+  ],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
